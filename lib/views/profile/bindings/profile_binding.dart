2626dbcbd8555362ef61cd32bc8ee82d7e2dfbbe
MustuBharmal <mustafabharmal10@gmail.com>
2024-02-25 12:14:29 +0530

    add profile screen

--- lib/views/profile/bindings/profile_binding.dart
+++ lib/views/profile/bindings/profile_binding.dart
@@ -4,7 +4,6 @@ import 'package:get/get.dart';
 class ProfileBinding extends Bindings {
   @override
   void dependencies() {
-    // TODO: implement dependencies
     Get.put(ProfileController());
   }
 }
